3b7e2c79bed13027414961e9cafe0206f9ce8a2f
John Aslinger <jaa4x@virginia.edu>
2017-09-17 19:21:51 -0400

    Updated to use lambda for errors

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,27 @@ import StringIO
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.johnaslinger.com')
-    build_bucket = s3.Bucket('portfoliobuild.johnaslinger.com')
-    build_bucket.download_file('portfoliobuild.zip', 'portfoliobuild.zip')
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:497588665354:deployPortfolioTopic')
 
-    print "Job done!"
+    try:
+        s3 = boto3.resource('s3')
+
+        portfolio_bucket = s3.Bucket('portfolio.johnaslinger.com')
+        build_bucket = s3.Bucket('portfoliobuild.johnaslinger.com')
+
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Portfolio Updated", Message="Portfolio project updated successfully.")
+    except:
+        topic.publish(Subject="Portfolio Update Failed", Message="Portfolio project was NOT updated.")
+        raise
+
     return 'Hello from Lambda'
