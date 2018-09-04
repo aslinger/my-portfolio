5abe23266a78c65f231974b56aec1c14778adb5a
John Aslinger <jaa4x@virginia.edu>
2018-09-04 08:20:24 -0400

    Improved UI

--- js/main.js
+++ js/main.js
@@ -12,10 +12,10 @@ const myWork = [
     title: "Email Pollster",
     href: "https://fast-ocean-28612.herokuapp.com/",
     desc:
-      "I started learning JS development through Udemy classes.  This project started out as Stephen Grider's Emaily full stack React/Node course. Since completing the class, I have added a variety of improvements. Some of them are: allowing custom responses, improved survey list UI, and survey deletion (including confirmation prompt).",
+      "The basis of this project is from Stephen Grider's Emaily full stack React/Node course. Since completing the class, I have added a variety of improvements in just one week. Some of them are: allowing custom responses, recipient response details, improved survey list UI (with charts), and survey deletion (including confirmation prompt).",
     image: {
-      desc: "example screenshot of a project involving code",
-      src: "images/example1.png",
+      desc: "",
+      src: "images/pollster.PNG",
       comment: ""
     }
   },
@@ -23,24 +23,11 @@ const myWork = [
     title: "Portfolio",
     href: "http://portfolio.johnaslinger.com",
     desc:
-      "This is a serverless React single-page app hosted on AWS. When commits are pushed to github, CodeBuild deploys the new website to S3 and emails me when the website is updated.",
+      "This is a serverless React single-page app hosted on AWS. When commits are pushed to GitHub, CodeBuild deploys the new website to S3 and emails me when the website is updated.",
     image: {
-      desc: "example screenshot of a project involving chemistry",
-      src: "images/example2.png",
-      comment: `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
-                    https://www.flickr.com/photos/ssoosay/4097410999`
-    }
-  },
-  {
-    title: "Work Example 3",
-    href: "http://www.example.com",
-    desc:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    image: {
-      desc: "example screenshot of a project involving cats",
-      src: "images/example3.png",
-      comment: `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
-                     https://www.flickr.com/photos/37287295@N00/2540855181`
+      desc: "",
+      src: "",
+      comment: ""
     }
   }
 ];
